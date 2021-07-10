90ad815638a8451da6ec6c070f7ee5a73103610e
Đỗ Thành Đạt <71697123+Ducanger@users.noreply.github.com>
2021-07-10 10:48:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,19 +18,19 @@ def get_info(url):
     #https://www.facebook.com/viettan/posts/10161553377150620
     #https://www.facebook.com/thongtinchinhphu/posts/4091030924307397 
     sleep(1)
-
+    """
     x_path_time = "/html/body/div[1]/div[3]/div[1]/div/div[2]/div[2]/div[2]/div[2]/div/div/div/div/div/div/div/div[1]/div/div[2]/div[1]/div[3]/div[1]/div/div/div[2]/div/div/div[2]/div/span[1]/span/a/abbr/span"
     time = driver.find_elements_by_xpath(x_path_time)
     info.append(time)
     print(time[0].text)
     sleep(0.5)
-
+    """
     x_path_post = "/html/body/div[1]/div[3]/div[1]/div/div[2]/div[2]/div[2]/div[2]/div/div/div/div/div/div/div/div[1]/div/div[2]/div[1]/div[3]/div[2]"
     post_message = driver.find_element_by_xpath(x_path_post).text
     info.append(post_message)
     print(post_message)
     sleep(0.5)
-
+    
     x_path_like = "/html/body/div[1]/div[3]/div[1]/div/div[2]/div[2]/div[2]/div[2]/div/div/div/div/div/div/div/div[1]/div/div[2]/div[2]/form/div/div[2]/div[1]/div/div[1]/a/span[2]/span/span"
     num_like = driver.find_element_by_xpath(x_path_like).text
     info.append(num_like)
